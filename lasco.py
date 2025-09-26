58acd238235b6eccb1dfd73cc5e2169233413b79
vmalkin <vaughnmalkin@gmail.com>
2025-09-26 14:33:11 +1200

    Begin refactor of LASCO downloader

--- lasco.py
+++ lasco.py
@@ -1,9 +1,12 @@
 import time
 import re
+
+import numpy as np
 import requests
 import os
 import global_config
 import standard_stuff
+import cv2
 
 def ring_system_bell():
     # rings the terminal bell.
@@ -76,13 +79,16 @@ def download_lasco(lasco_url, storage_folder):
 
 
 if __name__ == "__main__":
+    # Define the beginning of the current computation, and the start date of images we want to process.
     computation_start = time.time()
+    processing_start_date = int(computation_start - (86400 * 1))
+
     # If necessary, create the storage folder for lasco images. Otherwise, download latest images.
     storage_folder = global_config.folder_source_images + os.sep + "store_lasco_512"
     if not os.path.exists(storage_folder):
         os.makedirs(storage_folder)
 
-    tm = int(time.time())
+    tm = int(computation_start)
     ymd_now = int(standard_stuff.posix2utc(tm, "%Y%m%d"))
     ymd_old = ymd_now - 3
     year = standard_stuff.posix2utc(tm, "%Y")
@@ -97,28 +103,62 @@ if __name__ == "__main__":
     # #####################################################################################################
     enhanced_folder = global_config.folder_source_images + os.sep + "enhanced_lasco"
     analysis_folder = global_config.folder_source_images + os.sep + "analysis_lasco"
-    processing_start_date = int(time.time() - (86400 * 1))
+
     if not os.path.exists(enhanced_folder):
         os.makedirs(enhanced_folder)
     if not os.path.exists(analysis_folder):
         os.makedirs(analysis_folder)
+
     # BEGIN enhancesment of LASCO images, removing particle strikes and improve contrast.
     # Generate file list of current lasco images for the past 24 hours or other time.
-
+    file_list = os.listdir(storage_folder)
+    file_list.sort()
+    # The array item in list of current files has the format [posixtimestamp, filename]
+    current_files = []
+    for f in file_list:
+        ff = f.split("_")
+        timestamp = ff[0] + ff[1]
+        file_posix_time = standard_stuff.utc2posix(timestamp, '%Y%m%d%H%M')
+        # If the file timestamp is within our time interval
+        if file_posix_time >= processing_start_date:
+            dp = [file_posix_time, f]
+            current_files.append(dp)
     # Remove particle hits by taking the rolling median of 3 images, provided there is no time interval between any
-    # image greater than some empirically determined value. Create a new image from this.
+    # image greater than some empirically determined value. Create an array of new images from this.
+    time_threshold = 60 * 120
+    # The cleaned mage array stores [posixtimestamp, processed_image_binary]
+    cleaned_picture_array = []
+    if len(current_files) > 3:
+        for i in range(1, len(current_files) - 1):
+            temp_images = [current_files[i - 1], current_files[i], current_files[i + 1]]
+            a = np.array(temp_images)
+            tmp_times = a[:, 0]
+            tmp_images = a[:, 1]
+            if max(tmp_times) - min(tmp_times) <= time_threshold:
+                # load images and create new image based on median values
+                img_1 = cv2.imread(tmp_images[0], 0)
+                img_2 = cv2.imread(tmp_images[1], 0)
+                img_3 = cv2.imread(tmp_images[2], 0)
+                t = [img_1, img_2, img_3]
+                median_filtered_image = np.median(t, axis=0)
+                psx_timestamp = tmp_times[1]
+                dp = [psx_timestamp, median_filtered_image]
+                cleaned_picture_array.append(dp)
 
-    # If everything works out, apply image contrast/enhancement
+    # If everything works out, apply image contrast/enhancement to median image array
 
-    # At this point we can apply the local analyser to automatically calculate if a possible CME has been detected
+    # This array can be passed to a local analyser to automatically calculate if a possible CME has been detected
     # This creates a new set of images that have been convoluted and saved in the analysis folder.
 
-    # Add Dunedin Aurora stamp to image.
-    # Save image - FINISHED
+    # Add Dunedin Aurora stamp to images in array
+    # Write out images to files. FINISHED!
 
     # #####################################################################################################
     # End of LASCO download and analysis
     # #####################################################################################################
+
+    # FINAL HOUSEKEEPING - prune the size of the LASCO storage folder to some sane value. Perhaps the last 7 or 14 days?
+
     computation_end = time.time()
     elapsed_mins = round((computation_end - computation_start) / 60, 1)
     print(f"Processing time is {elapsed_mins} minutes")
