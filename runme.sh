d07a28d46ad6a338c8a9553e6ad46a06ec43b5c8
vmalkin <vaughnmalkin@gmail.com>
2026-08-26 09:45:46 +1200

    refactor of SUVI downloading

--- runme.sh
+++ runme.sh
@@ -2,11 +2,10 @@
 
 #while [ true ]; do
   .venv/bin/python lasco.py
-
   .venv/bin/python download_suvi.py
   .venv/bin/python process_suvi_diffs.py
   .venv/bin/python process_mlti_clr_suvi.py
-  .venv/bin/python process_histogram_analysis.py
+#  .venv/bin/python process_histogram_analysis_2.py
   .venv/bin/python process_movies.py
 
 #  python3 chs_main.py
